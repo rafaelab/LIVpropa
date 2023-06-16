c18eb13c15f5a02a67cb6609e8d7da1fb0c8330b
Rafael <rafaelab@users.noreply.github.com>
2023-06-16 11:40:18 +0200

    automatically copy to correct folder

--- helper/generateTables.sh
+++ helper/generateTables.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 
-# Download CRPropa3-data repository.
-# This will provide additional tools to compute the LIV tables.
+# download CRPropa3-data repository
+# this will provide additional tools to compute the LIV tables
 if [ -d "CRPropa3-data" ] 
 then
 	echo "CRPropa3-data already exists. Continuing..." 
@@ -13,5 +13,17 @@ fi
 
 
 
-# Compute the tables
+# compute the tables
+echo "Generating the interaction tables..."
 $PYTHON_EXECUTABLE computeElectromagneticInteractions.py
+
+# copy to the installation directory (assumed to be build/data)
+if [ -d "../build/data" ]
+then
+	cp -r ../data/PairProductionLIV/* ../build/data/PairProductionLIV/.
+	cp -r ../data/PairProductionLIV/* ../build/share/livpropa/PairProductionLIV/.
+	echo "Files copied to the appropriate folder." 
+else
+	echo "Installation path is not standard." 
+	echo "You should mannually copy the data files from ../data/ to the folder where LIVPropa was installed."
+fi
