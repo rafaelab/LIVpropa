c19bba5a2209888ef5c51213b6aa1a8c071e4091
Rafael <rafaelab@users.noreply.github.com>
2023-11-27 09:27:47 +0100

    proper implementation of Lorentz symmetry breaking

--- include/livpropa/Kinematics.h
+++ include/livpropa/Kinematics.h
@@ -2,15 +2,18 @@
 #define LIVPROPA_KINEMATICS_H
 
 #include <algorithm>
+#include <complex>
 #include <cstdlib>
 #include <cstring>
 #include <fstream>
 #include <unordered_map>
 
-
 #include <crpropa/Common.h>
 #include <crpropa/Referenced.h>
 #include <crpropa/Units.h>
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/Polynomials>
 
 #include "livpropa/UnitsAndConstants.h"
 
@@ -34,8 +37,10 @@ class Kinematics : public Referenced {
 		virtual std::string getShortIdentifier() const = 0;
 		virtual std::string getLocationData(std::vector<int> particles = {}) const = 0;
 		virtual double getSymmetryBreakingShift(const double& p) const = 0;
-		double computeEnergy2FromMomentum(const double& p, const double& m) const;
-		double computeEnergyFromMomentum(const double& p, const double& m) const;
+		virtual double computeMomentumFromEnergy(const double& E, const int& id) const = 0;
+		double computeEnergy2FromMomentum(const double& p, const int& id) const;
+		double computeEnergyFromMomentum(const double& p, const int& id) const;
+		
 };
 
 
@@ -50,8 +55,10 @@ class SpecialRelativity : public Kinematics {
 		SpecialRelativity();
 		std::string getShortIdentifier() const;
 		std::string getLocationData(std::vector<int> particles = {}) const;
-		double getSymmetryBreakingShift(const double& p) const;
-		double computeEnergyFromMomentum(const double& p, const double& m) const;
+		double getSymmetryBreakingShift(const double& p, const int& id) const;
+		// double computeEnergy2FromMomentum(const double& p, const int& id) const;
+		// double computeEnergyFromMomentum(const double& p, const int& id) const;
+		double computeMomentumFromEnergy(const double& E, const int& id) const;
 };
 
 
@@ -83,12 +90,12 @@ class MonochromaticLIV : public Kinematics {
 		std::string getLocationData(std::vector<int> particles = {}) const;
 		std::vector<int> getParticles()  const;
 		double getCoefficientForParticle(const int& particle) const;
-		double getSymmetryBreakingShift(const double& p) const;
-		double computeEnergyFromMomentum(const double& p, const double& m) const;
+		double getSymmetryBreakingShift(const double& p, const int& id) const;
+		double computeMomentumFromEnergy(const double& E, const int& id) const;
 };
 
 
 
 } // namespace livpropa
 
-#endif // LIVPROPA_LORENTZSYMMETRY_H
+#endif // LIVPROPA_KINEMATICS_H
