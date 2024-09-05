7855ed3f284cf335ae198007f8e009256d844e09
Rafael <rafaelab@users.noreply.github.com>
2024-09-06 01:13:16 +0200

    add sampling tools

---
+++ include/livpropa/Histogram.h
@@ -0,0 +1,175 @@
+#ifndef LIVPROPA_HISTOGRAM_H
+#define LIVPROPA_HISTOGRAM_H
+
+
+#include <string>
+#include <vector>
+
+#include <crpropa/Random.h>
+#include <crpropa/Referenced.h>
+
+#include "livpropa/Common.h"
+
+namespace livpropa {
+
+/**
+ @class Histogram1D
+ @brief Builds a one-dimensional histogram
+ @todo Use templates for the bin and content type
+ */
+class Histogram1D : public Referenced {
+	protected:
+		std::vector<double> edges;
+		std::vector<double> centres;
+		std::vector<double> widths;
+		std::vector<double> contents;
+		std::string scale;
+
+	public:
+		Histogram1D(double vmin, double vmax, int nBins, std::string scale = "lin") {
+			setScale(scale);
+			initBins(vmin, vmax, nBins);
+		}
+
+		~Histogram1D(){
+		}
+
+		void initBins(double vmin, double vmax, int nBins) {
+			// create empty histogram
+			if (scale == "log10") {
+				vmin = log10(vmin);
+				vmax = log10(vmax);
+				for (size_t i = 0; i < nBins + 1; i++) {
+					double v = vmin + i * (vmax - vmin) / (nBins);
+					edges.push_back(pow(10, v));
+				}
+				for (size_t i = 0; i < nBins; i++) {
+					centres.push_back(pow(10, (log10(edges[i + 1]) + log10(edges[i])) / 2.));
+				}
+			} else {
+				for (size_t i = 0; i < nBins + 1; i++) {
+					double v = vmin + i * (vmax - vmin) / (nBins);
+					edges.push_back(v);
+				}
+				for (size_t i = 0; i < nBins; i++) {
+					centres.push_back((edges[i + 1] + edges[i]) / 2.);
+				}
+			}
+
+			// bins start with no content
+			for (size_t i = 0; i < nBins; i++) {
+				widths.push_back(edges[i + 1] - edges[i]);
+				contents.push_back(0);
+			}
+		}
+
+		void setScale(std::string s) {
+			if (s == "log")
+				scale = "log10";
+
+			if (s != "log" && s != "log10" && s != "lin")
+				throw std::runtime_error("Unknown scale " + s + ".");
+
+			scale = s;
+		}
+
+		std::string getScale() const {
+			return scale;
+		}
+
+		std::vector<double> getBinEdges() const {
+			return edges;
+		}
+
+		std::vector<double> getBinWidths() const {
+			return widths;
+		}
+
+		std::vector<double> getBinCentres() const {
+			return centres;
+		}
+
+		std::vector<double> getBinContents() const {
+			return contents;
+		}
+
+		int getNumberOfBins() const {
+			return contents.size();
+		}
+
+		double getSample() const {
+			Random &random = Random::instance();
+			size_t bin = random.randBin(contents);
+			if (scale == "log") {
+				return pow(10, log10(edges[bin]) + random.rand() * log10(widths[bin]));
+			} else {
+				return edges[bin] + random.rand() * widths[bin];
+			}
+		}
+
+		void push(double v, double w = 1) {
+			std::vector<double>::const_iterator it = std::lower_bound(edges.begin(), edges.end(), v);
+			if (it == edges.begin() || it == edges.end())
+				return;
+
+			size_t idx = it - edges.begin();
+			contents[idx] += w; 
+		}
+
+		void normalise(double norm) {
+			for (size_t i = 0; i < getNumberOfBins(); i++) {
+				contents[i] /= norm;
+			}
+		}
+
+		double sum() {
+			double sum = 0;
+			for (size_t i = 0; i < getNumberOfBins(); i++) {
+				sum += contents[i];
+			}
+
+			return sum;
+		}
+
+		double integrate() {
+			double integral = 0;
+			if (scale == "log") {
+				for (size_t i = 0; i < getNumberOfBins(); i++) {
+					integral +=  (contents[i] / (edges[i + 1] - edges[i]) * log(10) * centres[i]);
+				}
+			} else {
+				for (size_t i = 0; i < getNumberOfBins(); i++) {
+					// integral += contents[i] / (edges[i] - edges[i - 1]);
+					integral += contents[i];
+				}
+			}
+
+			return integral;
+		}
+
+		void transformToPDF() {
+			double integral = integrate();
+			for (size_t i = 1; i < getNumberOfBins(); i++) {
+				// contents[i] /= (edges[i] - edges[i - 1]);
+				contents[i] /= integral;
+			}
+		}
+
+		void transformToCDF() {
+			for (size_t i = 1; i < getNumberOfBins(); i++) {
+				contents[i] += contents[i - 1];
+			}
+		}
+
+		void clear() {
+			for (size_t i = 0; i < getNumberOfBins(); i++) {
+				contents[i] = 0;
+			}
+		}
+
+};
+
+
+} // namespace livpropa
+
+#endif // LIVPROPA_HISTOGRAM_H
---
+++ include/livpropa/Sampler.h
@@ -0,0 +1,178 @@
+#ifndef LIVPROPA_SAMPLER_H
+#define LIVPROPA_SAMPLER_H
+
+#include <algorithm>
+#include <vector>
+
+#include <crpropa/Random.h>
+#include <crpropa/Referenced.h>
+
+#include "livpropa/Common.h"
+#include "livpropa/Histogram.h"
+
+
+
+namespace livpropa {
+
+ /**
+ @class SamplerEvents
+ @brief Abstract base class to handle individual events.
+ */
+class SamplerEvents: public Referenced {
+	public:
+		virtual double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const = 0;
+};
+
+
+/**
+ @class SamplerEventsUniform
+ @brief Throw away a fraction of the particles of a given type.
+ The sampling parameter controls whether all (sampling=1) or no (sampling=0) particles are accepted.
+ Note that the energy fraction is implemented through setters and getters, but it will constantly be modified.
+ This is because the structure of computeWeight should remain constant (or variadic arguments used). 
+ This will be done in the future.
+ */
+class SamplerEventsUniform: public SamplerEvents {
+	protected:
+		int particleId;
+		double sampling;
+
+	public:
+		SamplerEventsUniform(int particleId, double sampling);
+		void setSampling(double sampling);
+		void setParticleId(int particleId);
+		double getSampling() const;
+		int getParticleId() const;
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
+};
+
+
+/**
+ @class SamplerEventsEnergy
+ @brief Throw away a fraction of the particles of a given type according to a given distribution.
+ This distribution is defined from the derived classes.
+ The sampling parameter controls whether all (sampling=1) or no (sampling=0) particles are accepted.
+ This is essentially the implementation of the thinning of the EM interactions for index=1.
+ In this case, the thinning is defined as: thinning = 1 - sampling.
+ */
+class SamplerEventsEnergy : public SamplerEvents {
+	protected:
+		int particleId;
+		double sampling;
+
+	public:
+		SamplerEventsEnergy(int particleId, double sampling);
+		void setSampling(double sampling);
+		void setParticleId(int particleId);
+		double getSampling() const;
+		int getParticleId() const;
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
+		virtual double weightFunction(int id, double energy = 0, double energyFraction = 0, int counter = 0) const = 0;
+};
+
+
+/**
+ @class SamplerEventsEnergyFractionPowerLaw
+ @brief Throw away a fraction of the particles of a given type according to a power law associated with the energy fraction it takes.
+ The sampling parameter controls whether all (sampling=1) or no (sampling=0) particles are accepted.
+ This is essentially the implementation of the thinning of the EM interactions for index=1.
+ In this case, the thinning is defined as: thinning = 1 - sampling.
+
+ */
+class SamplerEventsEnergyFractionPowerLaw: public SamplerEventsEnergy {
+	protected:
+		int particleId;
+		double sampling;
+		double index;
+
+	public:
+		SamplerEventsEnergyFractionPowerLaw(double index, int particleId, double sampling);
+		void setIndex(double index);
+		double getIndex() const;
+		double weightFunction(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
+};
+
+
+/**
+ @class SamplerEventsEnergyFraction
+ @brief Throw away a fraction of the particles of a given type.
+ The sampling parameter controls whether all (sampling=1) or no (sampling=0) particles are accepted.
+ This is essentially the implementation of the thinning of the EM interactions. 
+ In this case, the thinning is defined as: thinning = 1 - sampling
+ */
+class SamplerEventsEnergyFraction: public SamplerEventsEnergyFractionPowerLaw {
+	public:
+		SamplerEventsEnergyFraction(int particleId, double sampling) : SamplerEventsEnergyFractionPowerLaw(1, particleId, sampling) {}
+};
+
+
+/**
+ @class SamplerEventsNull
+ @brief Useful dummy class to be used as initializer.
+ */
+class SamplerEventsNull : public SamplerEvents {
+	public:
+		SamplerEventsNull();
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
+};
+
+
+/**
+ @class SamplerEventsList
+ @brief List of objects of type "SamplerEvents".
+ */
+class SamplerEventsList : public SamplerEvents {
+	protected:
+		std::vector<ref_ptr<SamplerEvents>> samplers;
+
+	public:
+		SamplerEventsList();
+		SamplerEventsList(std::vector<ref_ptr<SamplerEvents>> samplers);
+		void add(SamplerEvents *samplers);
+		// inline void add(ref_ptr<SamplerEvents> SamplerEvents) {
+		// 	add(SamplerEvents.get());
+		// }
+		double computeWeight(int id, double energy = 0, double energyFraction = 0, int counter = 0) const;
+};
+
+
+/**
+ @class SamplerDistribution
+ @brief Builds a histogram and only after it is built samples from it.
+ */
+class SamplerDistribution : public Referenced {
+	public:
+		virtual std::vector<double> getSample(int nSamples) const = 0;
+		virtual int getSize() const = 0;
+		virtual ref_ptr<Histogram1D> getDistribution() const = 0;
+		virtual void transformToPDF() = 0;
+		virtual void transformToCDF() = 0;
+		virtual void append(const std::vector<double> &v) = 0;
+		virtual void push(const double &v) = 0;
+		virtual void clear() = 0; 
+};
+
+class SamplerDistributionUniform : public SamplerDistribution {
+	protected:
+		ref_ptr<Histogram1D> distribution;
+		int datasetSize;
+
+	public:
+		SamplerDistributionUniform(double vmin, double vmax, int nBins, std::string scale = "lin");
+		void setSize(int size);
+		int getSize() const;
+		void setDistribution(ref_ptr<Histogram1D> dist);
+		ref_ptr<Histogram1D> getDistribution() const;
+		std::vector<double> getSample(int nSamples) const;
+		void transformToPDF();
+		void transformToCDF();
+		void append(const std::vector<double> &v);
+		void push(const double &v);
+		void clear();
+};
+
+
+
+} // namespace livpropa
+
+#endif
