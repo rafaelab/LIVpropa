06c19897750687aafbbd497b4486801c94917162
Rafael <rafaelab@users.noreply.github.com>
2024-09-09 12:40:14 +0200

    add amalgamated header

---
+++ include/LIVpropa.h
@@ -0,0 +1,18 @@
+#ifndef LIVPROPA_H
+#define LIVPROPA_H
+
+
+#include "livpropa/Common.h"
+#include "livpropa/UnitsAndConstants.h"
+#include "livpropa/Histogram.h"
+#include "livpropa/Sampler.h"
+#include "livpropa/Data.h"
+#include "livpropa/Kinematics.h"
+#include "livpropa/InverseComptonScattering.h"
+#include "livpropa/PairProduction.h"
+#include "livpropa/PhotonDecay.h"
+#include "livpropa/VacuumCherenkov.h"
+
+
+
+#endif
