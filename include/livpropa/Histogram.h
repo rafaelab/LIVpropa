ac9d2da523a51e74af1976910fa102cc68ea80ca
Rafael <rafaelab@users.noreply.github.com>
2024-09-16 20:20:00 +0200

    fix sampling

--- include/livpropa/Histogram.h
+++ include/livpropa/Histogram.h
@@ -59,7 +59,12 @@ class Histogram1D : public Referenced {
 			contents.resize(n);
 		}
 
+		Histogram1D(Histogram1D&& h) noexcept {
+			*this = std::move(h);
+		}
+
 		~Histogram1D() {
+			// reset();
 		}
 
 		void initBins(double vmin, double vmax, unsigned int nBins) {
@@ -80,15 +85,11 @@ class Histogram1D : public Referenced {
 				initBinsLinear(vmin, vmax, nBins);
 			}
 
-			// bins starts with no content
-			for (size_t i = 0; i < nBins; i++) {
-				contents.push_back(0.);
-			}
 
 			edges.resize(nBins + 1);
 			centres.resize(nBins);
 			widths.resize(nBins);
-			contents.resize(nBins);
+			contents.resize(nBins, 0.);
 		}
 
 		void setScale(string s) {
@@ -99,6 +100,13 @@ class Histogram1D : public Referenced {
 				throw std::runtime_error("Unknown scale " + s + ".");
 
 			scale = s;
+		}
+
+		void setBinContents(vector<double> values) {
+			if (values.size() != contents.size())
+				throw runtime_error("Cannot set bin contents: number of values does not match number of bins.");
+
+			contents = values;
 		}
 
 		string getScale() const {
@@ -130,134 +138,6 @@ class Histogram1D : public Referenced {
 			return contents.size();
 		}
 
-		double drawRandom() const {
-			return getRandomValue(centres, contents, widths, scale);
-		}
-
-		double drawRandomInRange(const double& xMin, const double& xMax) const {
-			BinIterator binL = whichBin(xMin, true);
-			BinIterator binU = whichBin(xMax, false);
-
-			vector<double> edgesInRange;
-			vector<double> centresInRange; 
-			vector<double> contentsInRange;
-			vector<double> widthsInRange;
-			for (auto i = binL; i != binU; ++i) {
-				edgesInRange.push_back(*i);
-				centresInRange.push_back(centres[i - edges.begin()]);
-				contentsInRange.push_back(contents[i - edges.begin()]);
-				widthsInRange.push_back(widths[i - edges.begin()]);
-			}
-
-			return getRandomValue(centresInRange, contentsInRange, widthsInRange, scale);
-		}
-
-		double getSample() const {
-			double v = drawRandom();
-			return interpolate(v, centres, contents);
-		}
-
-		double getSampleInRange(const double& xMin, const double& xMax) const {
-			double v = drawRandomInRange(xMin, xMax);
-			return interpolate(v, centres, contents);
-		}
-
-		void push(double v, double w = 1) {
-			BinIterator it = whichBin(v);
-			if (it == edges.begin() || it == edges.end())
-				return;
-
-			size_t idx = it - edges.begin();
-			contents[idx] += w; 
-		}
-
-		void fill(const vector<double>& v, const vector<double>& w = {}) {
-			if (w.size() != 0) {
-				if (w.size() != v.size()) {
-					throw runtime_error("Cannot fill histogram: number of weights does not match number of values.");
-				}
-			} 
-			
-			for (size_t i = 0; i < v.size(); i++) {
-				push(v[i], w.size() == 0 ? 1 : w[i]);
-			}
-			
-		}
-
-		void normalise(double norm) {
-			for (size_t i = 0; i < getNumberOfBins(); i++) {
-				contents[i] /= norm;
-			}
-		}
-
-		double sum() {
-			double sum = 0;
-			for (size_t i = 0; i < getNumberOfBins(); i++) {
-				sum += contents[i];
-			}
-
-			return sum;
-		}
-
-		double integrate() {
-			double integral = 0;
-			if (scale == "log") {
-				for (size_t i = 0; i < getNumberOfBins(); i++) {
-					integral += contents[i] * (log10(edges[i + 1]) - log10(edges[i]));
-				}
-			} else {
-				for (size_t i = 0; i < getNumberOfBins(); i++) {
-					integral += contents[i] * widths[i];
-				}
-			}
-
-			return integral;
-		}
-
-		double interpolateAt(const double& v) const {
-			if (scale == "log10") {
-				vector<double> x;
-				vector<double> y;
-				for (size_t i = 0; i < getNumberOfBins(); i++) {
-					x.push_back(log10(centres[i]));
-					y.push_back(contents[i]);
-				}
-				return interpolate(log10(v), x, y);
-			} else {
-				return interpolate(v, centres, contents);
-			}
-		}
-
-		void transformToPDF() {
-			double totalSum = sum();
-			if (totalSum == 0) 
-				throw runtime_error("Cannot transform to PDF: total sum of bin contents is zero.");
-			
-			for (size_t i = 0; i < getNumberOfBins(); i++) {
-				contents[i] /= totalSum;
-			}
-		}
-
-		void transformToCDF() {
-			transformToPDF();
-			for (size_t i = 1; i < getNumberOfBins(); i++) {
-				contents[i] += contents[i - 1];
-			}
-		}
-
-		void clear() {
-			for (size_t i = 0; i < getNumberOfBins(); i++) {
-				contents[i] = 0;
-			}
-		}
-
-		void reset() {
-			contents.clear();
-			edges.clear();
-			centres.clear();
-			widths.clear();	
-		}
-
 		void setBinContent(size_t i, double v) {
 			contents[i] = v;
 		}
@@ -278,6 +158,105 @@ class Histogram1D : public Referenced {
 			return contents[i];
 		}
 
+		void push(double v, double w = 1) {
+			BinIterator it = whichBin(v);
+			if (it == edges.begin() or it == edges.end())
+				return;
+
+			size_t idx = it - edges.begin() - 1;
+			contents[idx] += w; 
+		}
+
+		void fill(const vector<double>& v, const vector<double>& w = {}) {
+			if ((w.size() != 0) and (w.size() != v.size())) {
+				throw runtime_error("Cannot fill histogram: number of weights does not match number of values.");
+			} 
+			for (size_t i = 0; i < v.size(); i++) {
+				push(v[i], w.size() == 0 ? 1. : w[i]);
+			}
+		}
+
+		void normalise(double norm) {
+			for (size_t i = 0; i < getNumberOfBins(); i++) {
+				contents[i] /= norm;
+			}
+		}
+
+		double sum() const {
+			double sum = 0;
+			for (size_t i = 0; i < getNumberOfBins(); i++) {
+				sum += contents[i];
+			}
+
+			return sum;
+		}
+
+		double integrate() const {
+			return getIntegral(contents, widths);
+		}
+
+		double interpolateAt(const double& v) const {
+			if (scale == "log10") {
+				vector<double> x;
+				vector<double> y;
+				for (size_t i = 0; i < getNumberOfBins(); i++) {
+					x.push_back(log10(centres[i]));
+					y.push_back(contents[i]);
+				}
+				return interpolate(log10(v), x, y);
+			} else {
+				return interpolate(v, centres, contents);
+			}
+		}
+
+		void transformToPDF() {
+			contents = getPDF(contents, widths);
+		}
+
+		void transformToCDF() {
+			contents = getCDF(contents, widths);
+		}
+
+		Histogram1D& computePDF() {
+			Histogram1D h = *this;
+			h.transformToPDF();
+			return h;
+		}
+
+		Histogram1D& computeCDF() {
+			Histogram1D h = *this;
+			h.transformToCDF();
+			return h;
+		}
+
+		double getSample() const {
+			Random& random = Random::instance();
+			size_t bin = random.randBin(contents);
+			return random.randUniform(edges[bin - 1], edges[bin]);
+		}
+
+		double getSample(const double& xMin, const double& xMax) const {
+			Random& random = Random::instance();
+			
+			BinIterator itL = whichBin(xMin, true);
+			BinIterator itU = whichBin(xMax, false);
+			size_t binL = itL - edges.begin();
+			size_t binU = itU - edges.begin();
+
+			return random.randUniform(edges[binL], edges[binU]);
+		}
+
+		void clear() {
+			std::fill(contents.begin(), contents.end(), 0.);
+		}
+
+		void reset() {
+			contents.clear();
+			edges.clear();
+			centres.clear();
+			widths.clear();	
+		}
+
 		double operator[](const size_t& i) const {
 			return getBinContent(i);
 		}
@@ -296,6 +275,17 @@ class Histogram1D : public Referenced {
 				contents.push_back(h.getBinContents()[i]);
 			}
 			edges.push_back(h.getBinEdges()[n]);
+
+			// edges = std::move(h.edges);
+			// centres = std::move(h.centres);
+			// widths = std::move(h.widths);
+			// contents = std::move(h.contents);
+			// scale = std::move(h.scale);
+
+			edges.resize(n + 1);
+			centres.resize(n);
+			widths.resize(n);
+			contents.resize(n);
 		
 			return *this;
 		}
@@ -310,6 +300,14 @@ class Histogram1D : public Referenced {
 				}
 			}
 			return binCentres;
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const Histogram1D& h) {
+			os << "Histogram1D: " << endl;
+			os << "  . scale: " << h.getScale() << endl;
+			os << "  . number of bins: " << h.getNumberOfBins() << endl;
+			os << "  . bin edges: " << h.getBinEdges().front() << ", " << h.getBinEdges().back() << endl;
+			return os;
 		}
 
 	private:
@@ -345,23 +343,45 @@ class Histogram1D : public Referenced {
 				return std::upper_bound(edges.begin(), edges.end(), v);
 		}
 		
-		static double getRandomValue(const vector<double>& binCentres, const vector<double>& binContents, const vector<double>& binWidths, const string& scale) {	
-			Random& random = Random::instance();
-			return interpolate(random.rand(), binContents, binCentres);
+		static double getIntegral(const vector<double>& y, const vector<double>& dx) {
+			if (y.size() != dx.size())
+				throw runtime_error("Cannot compute integral: number of y-values does not match number of bin widths.");
+
+			double integral = 0;
+			for (size_t i = 0; i < y.size(); i++) {
+				integral += y[i] * dx[i];
+			}
+
+			return integral;
+		}
+
+		static vector<double> getCDF(const vector<double>& y, const vector<double>& dx) {		
+			vector<double> cdf;
+			
+			double cum = 0;
+			for (size_t i = 1; i < y.size(); i++) {
+				// sum += y[i] * dx[i];
+				cum += y[i - 1];
+				cdf.push_back(cum);
+			}
+
+			return cdf;
+		}
+
+		static vector<double> getPDF(const vector<double>& y, const vector<double>& dx) {
+			double integral = getIntegral(y, dx);
+			
+			vector<double> pdf;
+			for (size_t i = 0; i < y.size(); i++) {
+				pdf.push_back(y[i] / dx[i] / integral);
+			}
+
+			return pdf;
 		}
 };
 
 
-// /** 
-// This function prints Histogram-type objects in a nice way
-// */
-// std::ostream& operator<<(std::ostream& os, const Histogram1D& h) {
-// 	os << "Histogram1D: " << endl;
-// 	os << "  . scale: " << h.getScale() << endl;
-// 	os << "  . number of bins: " << h.getNumberOfBins() << endl;
-// 	os << "  . bin edges: " << h.getBinEdges().front() << ", " << h.getBinEdges().back() << endl;
-// 	return os;
-// }
+
 
 
 } // namespace livpropa
