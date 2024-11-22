7a67cbb22cc33b4141655a0c8240c3b6e6fd70dc
Rafael <rafaelab@users.noreply.github.com>
2024-11-23 00:13:25 +0100

    redesign histogram

--- include/livpropa/Histogram.h
+++ include/livpropa/Histogram.h
@@ -11,6 +11,153 @@
 
 namespace livpropa {
 
+
+
+class AbstractBin1D: public Referenced {
+	protected:
+		double left;
+		double right;
+		double centre;
+
+	public:
+		virtual ~AbstractBin1D() = default;
+		void setCentre(double c);
+		void setEdges(double l, double r);
+		std::pair<double, double> getEdges() const;
+		double getLeftEdge() const;
+		double getRightEdge() const;
+		double getCentre() const;
+		double getWidth() const;
+		bool isInBin(const double& v) const;
+};
+
+
+class Bin1DLin : public AbstractBin1D {
+	public:
+		Bin1DLin(double l, double r) {
+			setEdges(l, r);
+			setCentre((l + r) / 2.);
+		}
+};
+
+
+enum class LogBase {
+	e,
+	two,
+	ten
+};
+
+inline double getLogBase(const LogBase& b) {
+	switch (b) {
+		case LogBase::e:
+			return M_E;
+		case LogBase::two:
+			return 2.;
+		case LogBase::ten:
+			return 10.;
+	}
+}
+
+
+template<LogBase B>
+class Bin1DLog : public AbstractBin1D {
+	public:
+		Bin1DLog(double l, double r) {
+			setEdges(l, r);
+
+			double b = getBase();
+			setCentre(pow(b, (logBase(l, b) + logBase(r, b)) / 2.));
+		}
+
+		double getBase() const { 
+			return getLogBase(B);
+		}
+};
+
+typedef Bin1DLog<LogBase::ten> Bin1DLog10;
+typedef Bin1DLog<LogBase::two> Bin1DLog2;
+typedef Bin1DLog<LogBase::e> Bin1DLogE;
+
+
+
+///////////////////////////////////
+
+class AbstractHistogram1D : public Referenced {
+	public:
+		typedef ref_ptr<AbstractBin1D> Bin;
+
+	protected:
+		vector<Bin> bins;
+		vector<double> contents;
+		vector<double> weights;
+		unsigned int nBins;
+
+	public:
+		virtual ~AbstractHistogram1D() = default;
+		bool isInRange(const double& v) const;
+		size_t getBinIndex(const double& v) const;
+		unsigned int getNumberOfBins() const;
+		double leftEdge() const;
+		double rightEdge() const;
+		void setBinContent(const size_t& idx, const double& value);
+		void setBinContents(const std::vector<double>& values);
+		double getBinContent(const size_t& i) const;
+		double getBinCentre(const size_t& i) const;
+		vector<double> getBinEdges() const;
+		vector<double> getBinCentres() const;
+		vector<double> getBinContents() const;
+		void push(const double& v, const double& w = 1);
+		void fill(const std::vector<double>& v, const std::vector<double>& w = {});
+		void normalise(double norm);
+		double sum() const;
+		double integrate() const;
+		double operator[](const size_t& i) const;
+		// virtual double interpolateAt(const double& v) const = 0;
+};
+
+template<class B>
+class Histogram1 : public AbstractHistogram1D {
+	public:
+		Histogram1();
+		Histogram1(double vMin, double vMax, unsigned int n);
+		~Histogram1();
+		void setBins(vector<Bin> bins);
+		bool isInRange(const double& v) const;
+		// virtual double interpolateAt(const double& v) const = 0;
+};
+
+typedef Histogram1<Bin1DLin> Histogram1DLin;
+typedef Histogram1<Bin1DLog10> Histogram1DLog10;
+
+
+// class Histogram1DLin : public Histogram1<Bin1DLin> {
+// 	public:
+// 		Histogram1DLin(double vMin, double vMax, unsigned int n);
+// 		double interpolateAt(const double& v) const;
+// 		// friend std::ostream& operator<<(std::ostream& os, const Histogram1DLin& h);
+// };
+
+// class Histogram1DLog10 : public Histogram1 {
+// 	public:
+// 		Histogram1DLog10(double vMin, double vMax, unsigned int n);
+// 		double interpolateAt(const double& v) const;
+// 		friend std::ostream& operator<<(std::ostream& os, const Histogram1DLog10& h);
+// };
+
+
+
+// typedef Histogram1<BinLinear1> Histogram1Dlin;
+// typedef Histogram1<BinLogarithmic1> Histogram1Dlog;
+
+// Histogram1<Bin1Linear> : public Histogram1<Bin1> {
+// 	public:
+// 		Histogram1(unsigned int n);
+// };
+
+
+
+
+
 class Histogram1D : public Referenced {
 	protected:
 		std::vector<double> edges;
@@ -19,19 +166,25 @@ class Histogram1D : public Referenced {
 		std::vector<double> cdf;
 		unsigned int nBins;
 		std::string scale;
+		bool logCDF;
+		double minLogCDF;
 
 	public:
-		Histogram1D(std::string scale = "lin");
-		Histogram1D(double vmin, double vmax, unsigned int n, std::string scale = "lin");
+		Histogram1D(std::string scale = "lin", bool logCDF = false, double minLogCDF = -14.);
+		Histogram1D(double vmin, double vmax, unsigned int n, std::string scale = "lin", bool logCDF = false, double minLogCDF = -14.);
 		Histogram1D(const Histogram1D& h);
 		Histogram1D(Histogram1D&& h) noexcept;
 		~Histogram1D();
 		void initBins(double vmin, double vmax, unsigned int n);
 		void prepareCDF();
 		void setScale(std::string s);
+		void setLogCDF(bool log);
+		void setMinimumLogCDF(double v);
 		void setNumberOfBins(unsigned int n);
 		void setBinContents(std::vector<double> values);
 		std::string getScale() const;
+		bool isCDFLogarithmic() const;
+		double getMinimumLogCDF() const;
 		std::vector<double> getBinEdges() const;
 		std::vector<double> getBinWidths() const;
 		std::vector<double> getBinCentres() const;
@@ -67,6 +220,11 @@ class Histogram1D : public Referenced {
 		static std::vector<double> computeBinCentresLinear(const std::vector<double>& binEdges, const std::string& scale);
 		static std::vector<double> computeBinCentresLog10(const std::vector<double>& binEdges, const std::string& scale);
 		static double getIntegral(const std::vector<double>& y, const std::vector<double>& dx);
+
+		// double samplingInverse(Random& random) const;
+		// double samplingImportance(Random& random, std::function<double(double)> weightFunction) const;
+		
+		// double samplingImportance() const;
 };
 
 } // namespace livpropa
--- src/Histogram.cc
+++ src/Histogram.cc
@@ -2,12 +2,309 @@
 
 namespace livpropa {
 
-Histogram1D::Histogram1D(string scale) {
-	setScale(scale);
+
+
+void AbstractBin1D::setEdges(double l, double r) {
+	left = l;
+	right = r;
 }
 
-Histogram1D::Histogram1D(double vmin, double vmax, unsigned int n, string scale) {
+void AbstractBin1D::setCentre(double c) {
+	centre = c;
+}
+
+pair<double, double> AbstractBin1D::getEdges() const {
+	return std::make_pair(left, right);
+}
+
+double AbstractBin1D::getLeftEdge() const {
+	return left;
+}
+
+double AbstractBin1D::getRightEdge() const {
+	return right;
+}
+
+double AbstractBin1D::getCentre() const {
+	return centre;
+}
+
+double AbstractBin1D::getWidth() const {
+	return right - left;
+}
+
+bool AbstractBin1D::isInBin(const double& v) const {
+	return v >= left and v < right;
+}
+
+
+
+bool AbstractHistogram1D::isInRange(const double& v) const {
+	if (v >= bins[0]->getLeftEdge() and v < bins[nBins - 1]->getRightEdge())
+		return true;
+
+	return false;
+}
+
+size_t AbstractHistogram1D::getBinIndex(const double& v) const {
+	if (! isInRange(v))
+		return -1;
+
+	for (size_t i = 0; i < nBins; i++) {
+		if (bins[i]->isInBin(v))
+			return i;
+	}
+
+	return -1;
+}
+
+unsigned int AbstractHistogram1D::getNumberOfBins() const {
+	return nBins;
+}
+
+double AbstractHistogram1D::leftEdge() const {
+	return bins[0]->getLeftEdge();
+}
+
+double AbstractHistogram1D::rightEdge() const {
+	return bins[nBins - 1]->getRightEdge();
+}
+
+double AbstractHistogram1D::getBinContent(const size_t& i) const {
+	return contents[i];
+}
+
+double AbstractHistogram1D::getBinCentre(const size_t& i) const {
+	return bins[i]->getCentre();
+}
+
+vector<double> AbstractHistogram1D::getBinEdges() const {
+	vector<double> edges;
+	for (size_t i = 0; i < nBins; i++) {
+		edges.push_back(bins[i]->getLeftEdge());
+	}
+	edges.push_back(bins[nBins - 1]->getRightEdge());
+
+	return edges;
+}
+
+vector<double> AbstractHistogram1D::getBinCentres() const {
+	vector<double> centres;
+	for (size_t i = 0; i < nBins; i++) {
+		centres.push_back(bins[i]->getCentre());
+	}
+
+	return centres;
+}
+
+vector<double> AbstractHistogram1D::getBinContents() const {
+	return contents;
+}
+
+void AbstractHistogram1D::push(const double& v, const double& w) {
+	size_t i = getBinIndex(v);
+	contents[i] += w;
+}
+
+void AbstractHistogram1D::fill(const vector<double>& v, const vector<double>& w) {
+	if (w.size() == 0) {
+		for (size_t i = 0; i < v.size(); i++) 
+			push(v[i]);
+	} else {
+		if (v.size() != w.size())
+			throw std::runtime_error("Number of values to fill bins must match number of weights.");
+
+		for (size_t i = 0; i < v.size(); i++)
+			push(v[i], w[i]);
+	}
+}
+
+void AbstractHistogram1D::setBinContent(const size_t& idx, const double& value) {
+	contents[idx] = value;
+}
+
+void AbstractHistogram1D::setBinContents(const vector<double>& values) {
+	if (values.size() != nBins)
+		throw std::runtime_error("Number of values must match number of bins.");
+
+	contents = values;
+}
+
+void AbstractHistogram1D::normalise(double norm) {
+	for (size_t i = 0; i < nBins; i++) {
+		contents[i] /= norm;
+	}
+}
+
+double AbstractHistogram1D::sum() const {
+	double s = 0;
+	for (size_t i = 0; i < nBins; i++) {
+		s += contents[i];
+	}
+
+	return s;
+}
+
+double AbstractHistogram1D::integrate() const {
+	double s = 0;
+	for (size_t i = 0; i < nBins; i++) {
+		double dx = bins[i]->getWidth();
+		s += contents[i] * dx;
+	}
+
+	return s;
+}
+
+double AbstractHistogram1D::operator[](const size_t& i) const {
+	return contents[i];
+}
+
+
+
+
+template<class B>
+Histogram1<B>::Histogram1() {
+	nBins = 0;
+}
+
+template<>
+Histogram1<Bin1DLin>::Histogram1(double vMin, double vMax, unsigned int n) {
+	nBins = n;
+
+	vector<Bin> bs;
+	double dx = (vMax - vMin) / n;
+	for (size_t i = 0; i < n; i++) {
+		double l = vMin + i * dx;
+		double r = vMin + (i + 1) * dx;
+		Bin b = new Bin1DLin(l, r);
+		bs.push_back(b);
+	}
+	setBins(bs);
+}
+
+template<>
+Histogram1<Bin1DLog10>::Histogram1(double vMin, double vMax, unsigned int n) {
+	nBins = n;
+
+	vector<Bin> bs;
+	double dx = (log10(vMax / vMin)) / n;
+	for (size_t i = 0; i < n; i++) {
+		double l = log10(vMin) + i * dx;
+		double r = log10(vMin) + (i + 1) * dx;
+		l = pow(10, l);
+		r = pow(10, r);
+		Bin b = new Bin1DLog10(l, r);
+		bs.push_back(b);
+	}
+	setBins(bs);
+}
+
+template<class B>
+Histogram1<B>::~Histogram1() {
+}
+
+template<class B>
+void Histogram1<B>::setBins(vector<Bin> b) {
+	bins = b;
+	nBins = bins.size();
+	contents.resize(nBins, 0.);
+}
+
+
+// Histogram1DLin::Histogram1DLin(double vMin, double vMax, unsigned int n) : Histogram1() {
+// 	nBins = n;
+
+// 	vector<Bin> bs;
+// 	double dx = (vMax - vMin) / n;
+// 	for (size_t i = 0; i < n; i++) {
+// 		double l = vMin + i * dx;
+// 		double r = vMin + (i + 1) * dx;
+// 		Bin b = new Bin1DLin(l, r);
+// 		bs.push_back(b);
+// 	}
+// 	setBins(bs);
+// }
+
+// double Histogram1DLin::interpolateAt(const double& x0) const {
+// 	if (! isInRange(x0))
+// 		throw std::runtime_error("Value is out of range and cannot be interpolated.");
+
+// 	size_t idx = getBinIndex(x0);
+
+// 	vector<double> vx;
+// 	vector<double> vy;
+// 	for (size_t i = idx - 1; i < idx + 2; i++) {
+// 		vx.push_back(bins[i]->getCentre());
+// 		vy.push_back(contents[i]);
+// 	}
+
+// 	return interpolate(x0, vx, vy);
+// }
+
+// std::ostream& operator<<(std::ostream& os, const Histogram1DLin& h) {
+// 	os << "Histogram1DLinD: " << endl;
+// 	os << "  . number of bins: " << h.getNumberOfBins() << endl;
+// 	os << "  . bin edges: " << h.leftEdge() << ", " << h.rightEdge() << endl;
+// 	return os;
+// }
+
+
+// Histogram1DLog10::Histogram1DLog10(double vMin, double vMax, unsigned int n) : Histogram1() {
+// 	nBins = n;
+
+// 	vector<Bin> bs;
+// 	double dx = (log10(vMax) - log10(vMin)) / n;
+// 	for (size_t i = 0; i < n; i++) {
+// 		double l = pow(10, log10(vMin) + i * dx);
+// 		double r = pow(10, log10(vMin) + (i + 1) * dx);
+// 		Bin b = new Bin1DLog10(l, r);
+// 		bs.push_back(b);
+// 	}
+
+// 	setBins(bs);
+// }
+
+// double Histogram1DLog10::interpolateAt(const double& x0) const {
+// 	if (! isInRange(x0))
+// 		throw std::runtime_error("Value is out of range and cannot be interpolated.");
+
+// 	size_t idx = getBinIndex(x0);
+// 	vector<double> vx;
+// 	vector<double> vy;
+// 	for (size_t i = idx - 1; i < idx + 2; i++) {
+// 		vx.push_back(log10(bins[i]->getCentre()));
+// 		vy.push_back(contents[i]);
+// 	}
+
+// 	return interpolate(log10(x0), vx, vy);
+// }
+
+// std::ostream& operator<<(std::ostream& os, const Histogram1DLog10& h) {
+// 	os << "Histogram1DLog10: " << endl;
+// 	os << "  . number of bins: " << h.getNumberOfBins() << endl;
+// 	os << "  . bin edges: " << h.leftEdge() << ", " << h.rightEdge() << endl;
+// 	return os;
+// }
+
+
+
+
+
+
+
+
+//////////////////
+
+Histogram1D::Histogram1D(string scale, bool logCDF, double minLogCDF) {
 	setScale(scale);
+	setLogCDF(logCDF);
+	setMinimumLogCDF(minLogCDF);
+}
+
+Histogram1D::Histogram1D(double vmin, double vmax, unsigned int n, string scale, bool logCDF, double minLogCDF) {
+	setScale(scale);
+	setLogCDF(logCDF);
+	setMinimumLogCDF(minLogCDF);
 	setNumberOfBins(n);
 	initBins(vmin, vmax, n);
 }
@@ -27,6 +324,10 @@ Histogram1D::Histogram1D(const Histogram1D& h) {
 	edges.resize(nBins + 1);
 	centres.resize(nBins);
 	contents.resize(nBins);
+
+	setScale(h.getScale());
+	setLogCDF(h.isCDFLogarithmic());
+	setMinimumLogCDF(h.getMinimumLogCDF());
 }
 
 Histogram1D::Histogram1D(Histogram1D&& h) noexcept {
@@ -60,24 +361,70 @@ void Histogram1D::prepareCDF() {
 	if (edges.size() < 2)
 		throw std::runtime_error("Number of bin edges must be greater than one.");
 
+	// ensure vector is empty
 	cdf.clear();
-	
+
 	// first entry of CDF is 0
 	cdf.push_back(0);
 
-	double cum = 0;
-	for (size_t i = 1; i < nBins; i++) {
-		cum += contents[i - 1];
-		cdf.push_back(cum);
-	}
+	if (logCDF) {
+		if (scale == "lin") {
+			double cum = 0;
+			for (size_t i = 0; i < nBins - 1; i++) {
+				cum += contents[i + 1];
+				cdf.push_back(cum);
+			}
+		} else if (scale == "log10") {
+			double cum = 0;
+			for (size_t i = 1; i < nBins; i++) {
+				double dx = edges[i] - edges[i - 1];
+				double y = log10(contents[i] + pow(10, minLogCDF - 1));
+				// double dy = contents[i] - contents[i - 1];
+				// double x = centres[i - 1];
+				// double y = contents[i - 1];
+				// y = log10(y);
+				// double w = y;
+				// cum += (y * dx / 2. * w);
+				cum += y * dx;
+				cdf.push_back(cum);
+			}
+		}
 
-	double tot = sum();
-	for (size_t i = 0; i < nBins; i++) {
-		cdf[i] /= tot;
-	}
+		double tot = sum();
+		for (size_t i = 0; i < nBins; i++) {
+			cdf[i] /= tot;
+		}
 
-	// last entry of CDF is 0
-	cdf.push_back(1);
+		// // do cdf->log(cdf)
+		// cdf[0] = minLogCDF;
+		// for (size_t i = 1; i < nBins; i++) {
+		// 	cdf[i] = log10(cdf[i]);
+		// }
+
+
+
+	} else {
+		if (scale == "lin") {
+			double cum = 0;
+			for (size_t i = 0; i < nBins - 1; i++) {
+				cum += contents[i + 1];
+				cdf.push_back(cum);
+			}
+		} else if (scale == "log10") {
+			double cum = 0;
+			for (size_t i = 1; i < nBins; i++) {
+				double dx = edges[i] - edges[i - 1];
+				double dy = contents[i] - contents[i - 1];
+				cum += (contents[i - 1] * dx / 2.);
+				cdf.push_back(cum);
+			}
+		}
+
+		double tot = sum();
+		for (size_t i = 0; i < nBins; i++) {
+			cdf[i] /= tot;
+		}
+	}
 
 }	
 
@@ -89,6 +436,14 @@ void Histogram1D::setScale(string s) {
 
 	if (s != "log" && s != "log10" && s != "lin")
 		throw std::runtime_error("Unknown scale " + s + ".");;
+}
+
+void Histogram1D::setLogCDF(bool log) {
+	logCDF = log;
+}
+
+void Histogram1D::setMinimumLogCDF(double v) {
+	minLogCDF = v;
 }
 
 void Histogram1D::setNumberOfBins(unsigned int n) {
@@ -103,6 +458,14 @@ void Histogram1D::setBinContents(vector<double> values) {
 
 string Histogram1D::getScale() const {
 	return scale;
+}
+
+bool Histogram1D::isCDFLogarithmic() const {
+	return logCDF;
+}
+
+double Histogram1D::getMinimumLogCDF() const {
+	return minLogCDF;
 }
 
 vector<double> Histogram1D::getBinEdges() const {
@@ -207,79 +570,57 @@ double Histogram1D::interpolateAt(const double& v) const {
 }
 
 double Histogram1D::getSample(bool binned) const {
-	if (cdf.empty())
-		throw runtime_error("Cannot get sample: CDF is empty. Call prepareCDF() first.");
+	// if (cdf.empty())
+	// 	throw runtime_error("Cannot get sample: CDF is empty. Call prepareCDF() first.");
 
 	Random& random = Random::instance();
 
-	if (binned) {
-		double r = random.rand();
+	// return samplingImportance(random);
 
-		auto it = std::lower_bound(cdf.begin(), cdf.end(), r);
-		size_t bin = std::distance(cdf.begin(), it) - 1;
+	// return samplingInverse(random);
 
-		size_t binL = bin;
-		size_t binU = bin + 1;
+	// if (logCDF) {
+	// 	return samplingImportance();
+	// } else {
+	// 	return interpolate(random.rand(), cdf, edges);
+	// }
 
-		return random.randUniform(edges[binL], edges[binU]);
-	} else {
-		double r = random.rand();
-		return interpolate(r, cdf, edges);
-	}
+	// if (logCDF) {
+	// 	// double r = random.randUniform(minLogCDF, 0.); 
+	// 	// r = pow(10, r);
+	// 	// double r = random.randExponential();
+	// 	double r = random.rand();
+	// 	double x = interpolate(r, cdf, edges);
+	// 	// size_t idx = getBinIndex(x);
+	// 	// return x / centres[idx];
+	// 	return x;
+
+	// } else {
+	// 	return interpolate(random.rand(), cdf, edges);
+	// }
 }
 
 double Histogram1D::getSample(const double& xMin, const double& xMax, bool binned) const {
 	Random& random = crpropa::Random::instance();
 
-	if (binned) {
-		double vMin = (xMin < edges.front()) ? edges.front() : xMin;
-		double vMax = (xMax > edges.back()) ? edges.back() : xMax;
+	vector<double> x;
+	vector<double> y;
 
-		size_t idxL = getBinIndex(vMin);
-		size_t idxU = getBinIndex(vMax) + 1;
+	if (xMin < centres.front() or xMax > centres.back())
+		throw runtime_error("Cannot get sample via inversion. Range is out of bounds (considering the bin centres).");
 
-		// possible bugs in first/last bins!
-
-		Histogram1D* h = new Histogram1D(edges[idxL], edges[idxU], idxU - idxL, scale);
-		for (size_t i = idxL; i < idxU; ++i) {
-			size_t idx = i - idxL;
-			h->setBinContent(idx, contents[i]);
+	x.push_back(xMin);
+	y.push_back(interpolate(xMin, centres, cdf));
+	for (size_t i = 0; i < getNumberOfBins(); i++) {
+		if (centres[i] > xMin and centres[i] < xMax) {
+			x.push_back(centres[i]);
+			y.push_back(cdf[i]);
 		}
-		h->prepareCDF();
-
-		double r = h->getSample();
-		
-		if (idxL == 0 or idxU == getNumberOfBins()) {
-			if (r < vMin or r > edges[idxL]) 
-				return random.randUniform(edges[idxL], vMin);
-			if (r > vMax or r < edges[idxU])
-				return random.randUniform(vMax, edges[idxU]);
-		}
-		delete h;
-
-		return r;
-	} else {
-		vector<double> x;
-		vector<double> y;
-
-		if (xMin < centres.front() or xMax > centres.back())
-			throw runtime_error("Cannot get sample via inversion. Range is out of bounds (considering the bin centres).");
-
-		x.push_back(xMin);
-		y.push_back(interpolate(xMin, centres, cdf));
-		for (size_t i = 0; i < getNumberOfBins(); i++) {
-			if (centres[i] > xMin and centres[i] < xMax) {
-				x.push_back(centres[i]);
-				y.push_back(cdf[i]);
-			}
-		}
-		x.push_back(xMax);
-		y.push_back(interpolate(xMax, centres, cdf));
-
-		double r = random.rand();
-
-		return interpolate(r, y, x);
 	}
+	x.push_back(xMax);
+	y.push_back(interpolate(xMax, centres, cdf));
+
+	return interpolate(random.rand(), y, x);
 }
 
 double Histogram1D::getSample(const std::pair<double, double>& range, bool binned) const {
@@ -384,6 +725,50 @@ double Histogram1D::getIntegral(const vector<double>& y, const vector<double>& d
 	return integral;
 }
 
+// double Histogram1D::samplingInverse(Random& random) const {
+// 	return interpolate(random.rand(), cdf, edges);
+// }
+
+// double Histogram1D::samplingImportance(Random& random, std::function<double(double)> weightFunction) const {
+
+//     // Sample from the proposal distribution
+//     double r = random.rand();
+//     auto it = std::lower_bound(proposal_cdf.begin(), proposal_cdf.end(), r);
+//     size_t idx = std::distance(proposal_cdf.begin(), it) - 1;
+
+//     if (idx >= nBins)
+//         idx = nBins - 1;
+
+//     double x1 = edges[idx];
+//     double x2 = edges[idx + 1];
+//     double y1 = contents[idx];
+//     double y2 = contents[idx + 1];
+
+//     if (scale == "log10") {
+//         y1 = log10(y1 + 1e-12);
+//         y2 = log10(y2 + 1e-12);
+//     }
+
+
+//     double t = (r - proposal_cdf[idx]) / (proposal_cdf[idx + 1] - proposal_cdf[idx]);
+//     double x = x1 + t * (x2 - x1);
+
+//     // Compute the importance weight using the provided weight function
+//     double proposal_density = (y1 + t * (y2 - y1)) / total;
+//     double target_density = weightFunction(x);
+
+//     double weight = target_density / proposal_density;
+
+//     // Accept or reject the sample based on the weight
+//     double acceptance = random.rand();
+//     if (acceptance <= weight) {
+//         return x;
+//     } else {
+//         // If rejected, recursively call the function to get a new sample
+//         return samplingImportance(weightFunction);
+//     }
+// }
+
 
 std::ostream& operator<<(std::ostream& os, const Histogram1D& h) {
 	os << "Histogram1D: " << endl;
@@ -395,6 +780,4 @@ std::ostream& operator<<(std::ostream& os, const Histogram1D& h) {
 
 
 
-
-
-} // namespace livpropa
+} // namespace liveprop
